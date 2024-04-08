1f8756112ae687c769112fbd0ad0739f95fb86ed
exelllolr <69623659+exelllolr@users.noreply.github.com>
2024-04-08 11:26:26 +0300

    Add files via upload

---
+++ Exam-Alekseev/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Exam_Alekseev.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Windows.Application;
+
+namespace Exam_Alekseev
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities db = new Entities();
+    }
+}
+
---
+++ Exam-Alekseev/Pages/RegPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam_Alekseev.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        User contexntUser;
+        public RegPage()
+        {
+            InitializeComponent();
+            CBRole.ItemsSource = App.db.Role.ToList();
+            contexntUser = new User();
+            DataContext = contexntUser;
+
+        }
+
+        private void BBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void BReg_Click(object sender, RoutedEventArgs e)
+        {
+            App.db.Users.Add(contexntUser);
+            App.db.SaveChanges();
+            NavigationService.GoBack();
+        }
+    }
+}
